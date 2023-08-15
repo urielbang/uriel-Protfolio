7048a9c006d8d38f676e0a0e8b9ec5d09ca0a370
urielbang <uri32655@gmail.com>
2023-08-15 11:10:14 +0300

    cube

--- cubeApp.js
+++ cubeApp.js
@@ -1,1 +1,14 @@
-let num = 5 > 10 ? true : false;
+const cube = document.querySelector(".cube");
+let mouseX = 0;
+let mouseY = 0;
+const rotationValue = 270;
+
+const handleMouseMove = (event) => {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+  rotateX = -(mouseY / window.innerHeight - 0.5) * rotationValue;
+  rotateY = (mouseX / window.innerWidth - 0.5) * rotationValue;
+  cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+};
+
+window.addEventListener("mousemove", handleMouseMove);
